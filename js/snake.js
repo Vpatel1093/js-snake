4c513717afc355334162e4076b49457fa36b2e22
Vishal <v.patel1093@gmail.com>
2017-09-21 18:07:02 -0400

    create function for randomly placed apple

--- js/snake.js
+++ js/snake.js
@@ -4,11 +4,22 @@ function drawGrid() {
 
   for (var i = 0; i < rows; i++) {
     for (var j = 0; j < columns; j++) {
-      $('#grid').append('<div id="row' + i + '-col' + j + '" class="cell empty"</div>');
+      $('#grid').append('<div id="row' + i + '-col' + j + '" class="cell empty"></div>');
     };
   };
 };
 
+function placeFruit() {
+  var fruitCell;
+  do {
+    var fruitRow = Math.floor(Math.random()*40) +1;
+    var fruitColumn = Math.floor(Math.random()*40) +1;
+    fruitCell = $('#row' + fruitRow + '-col' + fruitColumn);
+  } while(fruitCell.hasClass('snake'));
+  fruitCell.addClass('apple');
+};
+
 $(document).ready(function snake() {
   drawGrid();
+  placeFruit();
 });
