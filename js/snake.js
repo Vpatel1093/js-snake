8a4d43f396e0c2db5801a1bbe46a6b083c004f11
Vishal <v.patel1093@gmail.com>
2017-09-22 14:29:52 -0400

    create game loop and begin function for moving snake

--- js/snake.js
+++ js/snake.js
@@ -3,6 +3,7 @@ var snake = [];
 var score = 0;
 var fruitCell = [];
 var gameInProgress = false;
+var currentDirection = '';
 const left = 37;
 const up = 38;
 const right = 39;
@@ -37,6 +38,7 @@ function resetGame() {
   placeFruit();
   score = 0;
   gameInProgress = false;
+  currentDirection = '';
 }
 
 function placeFruit() {
@@ -48,10 +50,54 @@ function placeFruit() {
   fruitCell.addClass('apple');
 };
 
-function playGame(direction) {
-  console.log(direction);
+function playGame() {
+  do {
+    setTimeout(moveSnake(), 100);
+  } while(gameInProgress);
 };
 
+function moveSnake() {
+  $(document).keydown(function(newDirection) {
+    currentDirection = newDirection.which;
+  });
+
+  var move = function(currentDirection) {
+    var snakeHead = snake[0];
+    switch(currentDirection){
+      case 37:
+        var nextCellCoords = [snakeHead[0], snakeHead[1]-1];
+        var nextCell = $('#row' + nextCellCoords[0] + '-col' + nextCellCoords[1]);
+        break;
+      case 38:
+        var nextCellCoords = [snakeHead[0]-1, snakeHead[1]];
+        var nextCell = $('#row' + nextCellCoords[0] + '-col' + nextCellCoords[1]);
+        break;
+      case 39:
+        var nextCellCoords = [snakeHead[0], snakeHead[1]+1];
+        var nextCell = $('#row' + nextCellCoords[0] + '-col' + nextCellCoords[1]);
+        break;
+      case 40:
+        var nextCellCoords = [snakeHead[0]+1, snakeHead[1]];
+        var nextCell = $('#row' + nextCellCoords[0] + '-col' + nextCellCoords[1]);
+        break;
+    };
+
+
+    // Update snake when next cell empty
+    if (nextCell.hasClass('empty')) {
+      snake.unshift(nextCellCoords);
+      nextCell.removeClass('empty').addClass('snake');
+      var snakeTail = snake.pop();
+      $('#row' + snakeTail[0] + '-col' + snakeTail[1]).removeClass('snake').addClass('empty');
+    };
+  };
+
+  if (37 <= currentDirection && currentDirection <= 40) {
+    move(currentDirection);
+  };
+
+  gameInProgress = false;
+};
 
 $(document).ready(function initGame() {
   initGrid();
@@ -59,13 +105,11 @@ $(document).ready(function initGame() {
   placeFruit();
 
   $(document).keydown(function startGame(key) {
-    direction = key.which
     if (!gameInProgress) {
-      console.log('a' + direction)
-      if (direction === left || direction === up || direction === right || direction === down) {
+      currentDirection = key.which;
+      if (currentDirection === left || currentDirection === up || currentDirection === right || currentDirection === down) {
         gameInProgress = true;
-        playGame(direction);
-        console.log('b' +direction);
+        playGame();
       };
     };
   });
